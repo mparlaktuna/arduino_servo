5eb05275e6f53f7768f56b5114bdf154f1fad65d
Mustafa Parlaktuna <mparlaktuna@gmail.com>
2015-03-16 22:51:51 +0200

    Added speed control

--- README.md
+++ README.md
@@ -3,6 +3,9 @@ This  is for controling rc servo motors with arduino. 16 channel pwm controller 
 adafruit is used on the arduino uno. Also ROS is used for communication.
 
 Usage codes:
+to compile and upload to arduino:
+catkin_make arudino_servo_firmware_motor_servo-upload
 
-rosrun rosserial_python serial_node.py _port:=/dev/ttyUSB0
+
+rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0
 rostopic pub servo std_msgs/UInt16  <angle>
--- firmware/servo.cpp
+++ firmware/servo.cpp
@@ -5,21 +5,33 @@
 #include <std_msgs/UInt16.h>
 #include <std_msgs/UInt16MultiArray.h>
 
+// create position and speed values, maybe make them into a struck in the future
+int motor_positions[11];
+int speeds[11];
+int motor_goal_positions[11];
+
 
 ros::NodeHandle  nh;
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
 
 void servo_cb( const std_msgs::UInt16MultiArray& cmd_msg){
-  pwm.setPWM(cmd_msg.data[0], 0, cmd_msg.data[1]);
+	motor_goal_positions[cmd_msg.data[0]] = cmd_msg.data[1];
+	speeds[cmd_msg.data[0]] = cmd_msg.data[2];
 }
-
 
 ros::Subscriber<std_msgs::UInt16MultiArray> sub("servo", servo_cb);
 
+
 void setup(){
-  Serial.begin(9600);
-  Serial.println("16 channel Servo test!");
+
+	for (int x = 0; x < 11  ; x++)
+	{
+//		initial positions are the middle positions
+		motor_positions[x]=200;
+		motor_goal_positions[x]=200;
+		speeds[x] = 5;
+	}
 
   pwm.begin();
 
@@ -30,6 +42,12 @@ void setup(){
 }
 
 void loop(){
-  nh.spinOnce();
-  delay(1);
+	for (int x = 0; x < 11; x++)
+		{
+			//calculate error and next position depending on speed
+			motor_positions[x] = motor_positions[x] + (motor_goal_positions[x]-motor_positions[x])/speeds[x];
+			pwm.setPWM(x, 0, motor_positions[x]);
+		}
+	nh.spinOnce();
+	delay(1);
 }
